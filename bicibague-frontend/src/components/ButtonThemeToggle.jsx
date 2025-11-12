9da596b5f06475b789a92f199869657fe424e47d
Saregom <santi26reyes@gmail.com>
2025-11-11 21:14:58 -0500

    theme toggle button transition added

--- bicibague-frontend/src/components/ButtonThemeToggle.jsx
+++ bicibague-frontend/src/components/ButtonThemeToggle.jsx
@@ -1,15 +1,22 @@
 // hooks
 import { useTheme } from '@hooks/useTheme';
+import { useScrollDirection } from '@hooks/useScrollDirection';
 // icons
-import { FaRegMoon, FaRegSun } from 'react-icons/fa6'
+import { FaRegMoon, FaRegSun } from 'react-icons/fa6';
 // styles
 import './components.scss';
 
 export const ButtonThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
+  const scrollDirection = useScrollDirection();
 
   return (
-    <button className="theme-toggle" onClick={toggleTheme}>
+    <button
+      className={`theme-toggle ${
+        scrollDirection === 'down' ? 'theme-toggle--hidden' : ''
+      }`}
+      onClick={toggleTheme}
+    >
       {theme === 'light' ? (
         <FaRegMoon className="theme-icon" />
       ) : (
